5a52219b2d05e9fe5ee78d984e88138806f95ec6
Justin Meiners <justin.meiners@gmail.com>
2019-04-21 23:00:49 -0600

    start on calculus on manifolds

---
+++ calculus-on-manifolds/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# thanks to
+# https://gist.github.com/lelandbatey/8677901
+convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -type Grayscale -resize 768\> "$2"
