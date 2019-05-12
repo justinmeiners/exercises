66e441ad56a5f59bc3b9668494c6db776d33d197
Justin Meiners <justin.meiners@gmail.com>
2019-05-11 22:40:30 -0600

    more

--- calculus-on-manifolds/cleanup.sh
+++ calculus-on-manifolds/cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # thanks to
 # https://gist.github.com/lelandbatey/8677901
-convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -type Grayscale -resize 768\> "$2"
+FNAME="${1%.*}"
+convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -type Grayscale -resize 768\> "$FNAME.gif"
