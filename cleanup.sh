20700aaf55f2849619a099879cd2e4d38c9f6da0
Justin Meiners <justin.meiners@gmail.com>
2020-11-26 12:02:25 -0700

    move

---
+++ cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# thanks to
+# https://gist.github.com/lelandbatey/8677901
+FNAME="${1%.*}"
+convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 0x1 -channel RBG -level 60%,91%,0.1 -type Grayscale "$FNAME.png"
